8a4236c919e2518355d188f398cc93eaf23b950a
YYYukiyamazakIII <fairy_0530_0710@yahoo.co.jp>
2022-03-23 06:40:22 +0900

    userモデル単体テストの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -52,8 +52,20 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include 'Password is too long (maximum is 128 characters)'
       end
-      it 'passwordは半角英数字混合でなければ登録できない' do
+      it 'passwordは半角英字だけでは登録できない' do
         @user.password = Faker::Lorem.characters(number: 6, min_alpha: 6)
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include 'Password には英字と数字の両方を含めて設定してください'
+      end
+      it 'passwordは半角数字だけでは登録できない' do
+        @user.password = Faker::Lorem.characters(number: 6, min_numeric: 6)
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include 'Password には英字と数字の両方を含めて設定してください'
+      end
+      it 'passwordは全角文字を含むと登録できない' do
+        @user.password = "#{Faker::Lorem.characters(number: 6..125, min_alpha: 1, min_numeric: 1)} １２３"
         @user.password_confirmation = @user.password
         @user.valid?
         expect(@user.errors.full_messages).to include 'Password には英字と数字の両方を含めて設定してください'
