72ad6e8b57236483010b72d7916b7134918edc6e
YYYukiyamazakIII <fairy_0530_0710@yahoo.co.jp>
2022-04-03 17:05:50 +0900

    move_to_indexの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,7 +46,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     redirect_to root_path if current_user.id != @item.user.id
   end
 end
