4a327236fb4d8c3c09fa52759c8bfac48d6b8b02
YYYukiyamazakIII <fairy_0530_0710@yahoo.co.jp>
2022-04-17 14:45:06 +0900

    環境変数の設定

--- spec/models/purchased_item_shipping_address_spec.rb
+++ spec/models/purchased_item_shipping_address_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe PurchasedItemShippingAddress, type: :model do
+  
   describe '商品購入機能' do
     before do
       user = FactoryBot.create(:user)
@@ -8,7 +9,7 @@ RSpec.describe PurchasedItemShippingAddress, type: :model do
       @purchased_item_shipping_address = FactoryBot.build(:purchased_item_shipping_address, user_id: user.id, item_id: item.id)
       sleep 0.1
     end
-    
+
     context '商品購入ができるとき' do
       it 'すべての項目が入力されていれば購入できる' do
         expect(@purchased_item_shipping_address).to be_valid
