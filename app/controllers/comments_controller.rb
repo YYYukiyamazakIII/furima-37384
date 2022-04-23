027923534953ae8d2ee82153f424d50070b83fb6
YYYukiyamazakIII <fairy_0530_0710@yahoo.co.jp>
2022-04-23 10:04:19 +0900

    ActionCableの実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,13 @@
 class CommentsController < ApplicationController
-
   def create
     @comment = Comment.new(comment_params)
-    if @comment.save
-      redirect_to item_path(params[:item_id])
-    end
+    @item = Item.find(params[:item_id])
+    CommentChannel.broadcast_to @item, { comment: @comment, user: @comment.user } if @comment.save
   end
 
   private
-  def comment_params
-    params.require(:comment).permit(:text).merge(user_id:current_user.id, item_id: params[:item_id])
-  end
 
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
