5a842d6d7d6527a377e76ade9482081add4e74e5
YYYukiyamazakIII <fairy_0530_0710@yahoo.co.jp>
2022-04-25 06:45:47 +0900

    good_itemsテーブルの保存

--- app/controllers/good_items_controller.rb
+++ app/controllers/good_items_controller.rb
@@ -1,10 +1,12 @@
 class GoodItemsController < ApplicationController
 
   def create
-
+    unless GoodItem.where(user_id:current_user.id, item_id: params[:item_id]).present?
+      GoodItem.create(user_id:current_user.id, item_id: params[:item_id])
+    end
   end
 
   def destroy
-
   end
+
 end
