d6a92559aecb8a4e0788c0a0fb9845e03c9088f0
YYYukiyamazakIII <fairy_0530_0710@yahoo.co.jp>
2022-06-15 09:38:11 +0900

    リファクタリング

--- app/controllers/good_items_controller.rb
+++ app/controllers/good_items_controller.rb
@@ -1,15 +1,16 @@
 class GoodItemsController < ApplicationController
   def create
-    unless GoodItem.where(user_id: current_user.id, item_id: params[:item_id]).present?
+    if GoodItem.where(user_id: current_user.id, item_id: params[:item_id]).present?
+      good_item = GoodItem.where(user_id: current_user.id, item_id: params[:item_id])[0]
+      render json: { judge: "false", itemId: good_item.item_id, goodItemId: good_item.id }
+    else
       GoodItem.create(user_id: current_user.id, item_id: params[:item_id])
-      good_item_count = GoodItem.where(item_id: params[:item_id]).count
-      render json: { good_item_count: good_item_count }
+      render json: { goodItemCount: GoodItem.where(item_id: params[:item_id]).count }
     end
   end
 
   def destroy
     GoodItem.find(params[:id]).destroy
-    good_item_count = GoodItem.where(item_id: params[:item_id]).count
-    render json: { good_item_count: good_item_count }
+    render json: { goodItemCount: GoodItem.where(item_id: params[:item_id]).count }
   end
 end
--- app/javascript/good_item.js
+++ app/javascript/good_item.js
@@ -1,40 +1,33 @@
 function create (){
   const submit = document.getElementById("favorite-btn");
-  const id = document.getElementById("info_item_id")
   submit.addEventListener('click', (e) => {
     e.preventDefault();
     const form = document.getElementById("form");
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
-    XHR.open("POST", `${id.value}/good_items`, true);
+    XHR.open("POST", form.action, true);
     XHR.responseType = "json";
     XHR.send(formData);
     XHR.onload = () => {
-      const good_item_count = XHR.response.good_item_count;
+      if( XHR.response.judge == "false" ){
+        destroy(formData, XHR)
+        return;
+      };
       const value = document.getElementById("good_item_count")
-      value.innerHTML = `お気に入り ${good_item_count}`
+      value.innerHTML = `お気に入り ${XHR.response.goodItemCount}`
     };
   });
 };
+
+function destroy(formData, XHR){
+  XHR.open("DELETE", `/items/${XHR.response.itemId}/good_items/${XHR.response.goodItemId}`, true);
+  XHR.responseType = "json";
+  XHR.send(formData);
+  XHR.onload = () => {
+    const value = document.getElementById("good_item_count")
+    value.innerHTML = `お気に入り ${XHR.response.goodItemCount}`
+  };
+};
+
 window.addEventListener('load',create);
 
-function destroy(){
-  const submitDestroy = document.getElementById("favorite-btn-destroy")
-  const id = document.getElementById("info_id")
-  const item_id = document.getElementById("info_item_id")
-  submitDestroy.addEventListener('click',(e)=>{
-    e.preventDefault();
-    const form = document.getElementById("form-destroy");
-    const formData = new FormData(form);
-    const XHR = new XMLHttpRequest();
-    XHR.open("DELETE", `/items/${item_id.value}/good_items/${id.value}`, true);
-    XHR.responseType = "json";
-    XHR.send(formData);
-    XHR.onload = () => {
-      const good_item_count = XHR.response.good_item_count;
-      const value = document.getElementById("good_item_count")
-      value.innerHTML = `お気に入り ${good_item_count}`
-    }
-  });
-};
-window.addEventListener('load',destroy);
